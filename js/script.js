998fe0a44fac4b96e24bbe3f1da74810440d7b88
Danko-M <dankomilutinovic@gmail.com>
2016-11-04 18:07:38 +0100

    mobile experiment

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ var carousel01Opt = {
   navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>',
   					'<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
   responsiveClass: true,
+  touchDrag: false,
   responsive: {
     0: {
       items: 1
